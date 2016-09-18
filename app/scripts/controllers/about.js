dd876243a337cc1c4652207c38ba1c46af07e73c
Markus Wagner <Markus Wagner>
2016-09-18 09:05:52 +0100

    Base

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nerdreflexApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nerdreflexApp
+ */
+angular.module('nerdreflexApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nerdreflexApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nerdreflexApp
+ */
+angular.module('nerdreflexApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
